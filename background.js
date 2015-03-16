118d46872b45591509a66cd09f0d585ac04755e5
Blaine Bublitz <blaine@iceddev.com>
2015-03-15 23:16:35 -0700

    make it a chrome app

---
+++ background.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Listens for the app launching then creates the window
+chrome.app.runtime.onLaunched.addListener(function(){
+  var width = 1000;
+  var height = 500;
+
+  chrome.app.window.create('index.html', {
+    id: 'chromebots',
+    state: 'maximized',
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screen.availWidth - width) / 2),
+      top: Math.round((screen.availHeight - height) / 2)
+    }
+  });
+});
