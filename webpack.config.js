d321a63a7b4c752f267c2047438ff5a6eacf9833
Blaine Bublitz <blaine@iceddev.com>
2015-03-15 23:08:15 -0700

    add initial js and devDeps

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+
+var shouldWatch = (process.argv.indexOf('--watch') !== -1);
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader?optional=runtime'
+      }
+    ]
+  },
+  resolveLoader: {
+    // support loaders against linked modules
+    root: path.join(__dirname, 'node_modules')
+  },
+  resolve: {
+    // support aliases against linked modules
+    root: path.join(__dirname, 'node_modules')
+  },
+  // crash on invalid bundle
+  bail: true,
+  watch: shouldWatch
+};
