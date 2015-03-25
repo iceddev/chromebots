157c004c38efa60927d07e2423b8cf24cb6aa7b5
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-03-25 09:45:52 -0700

    missing test stuff

---
+++ test/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const expect = require('expect');
+
+describe('chromebots', function(){
+
+  it('has an example test', function(done){
+    expect(true).toEqual(true);
+    done();
+  });
+});
